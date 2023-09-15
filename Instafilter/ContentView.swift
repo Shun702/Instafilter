fc0ab28c5dc2e7a8ca22043044c4a597bbe714b3
Shun702 <138392252+Shun702@users.noreply.github.com>
2023-09-15 13:24:55 +0100

    initial commit
    initial code

---
+++ Instafilter/ContentView.swift
@@ -0,0 +1,153 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Shun Le Yi Mon on 24/08/2023.
+//
+
+import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
+struct ContentView: View {
+    @State private var image: Image? {
+        didSet {
+            imageProcessEnanbled = false
+        }
+    }
+    @State private var filterIntensity = 0.5
+    @State private var radiusIntensity = 0.5
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    @State private var processedImage: UIImage?
+    @State private var showingFilterSheet = false
+    
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    
+    @State private var imageProcessEnanbled = true
+    let context = CIContext()
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(.secondary)
+
+                    Text("Tap to select a picture")
+                        .foregroundColor(.white)
+                        .font(.headline)
+
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                }
+                .onTapGesture {
+                    showingImagePicker = true
+                }
+
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity) { _ in
+                            applyProcessing()
+                        }
+                }
+                .padding(.vertical)
+                HStack {
+                    Text("Radius")
+                    Slider(value: $radiusIntensity)
+                        .onChange(of: radiusIntensity) { _ in
+                            applyProcessing()
+                        }
+                }
+                .padding(.vertical)
+
+                HStack {
+                    Button("Change Filter") {
+                        showingFilterSheet = true
+                    }
+
+                    Spacer()
+
+                    Button("Save", action: save).disabled(imageProcessEnanbled)
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
+            .sheet(isPresented: $showingImagePicker) {
+                ImagePicker(image: $inputImage)
+            }
+            .confirmationDialog("Select a filter", isPresented: $showingFilterSheet) {
+                Button("Crystallize") { setFilter(CIFilter.crystallize()) }
+                Button("Edges") { setFilter(CIFilter.edges()) }
+                Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }
+                Button("Pixellate") { setFilter(CIFilter.pixellate()) }
+                Button("Sepia Tone") { setFilter(CIFilter.sepiaTone()) }
+                Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
+                Button("Vignette") { setFilter(CIFilter.vignette()) }
+                Button("Sharpen") { setFilter(CIFilter.sharpenLuminance()) }
+                Button("Box Blur") { setFilter(CIFilter.boxBlur())
+                }
+                Button("Hole Distortion") {
+                    setFilter(CIFilter.holeDistortion())
+                }
+                //Button("Cancel", role: .cancel) {}
+            }
+            .onChange(of: inputImage) { _ in loadImage() }
+            .onAppear(perform: save)
+        }
+    }
+    
+    func save() {
+        guard let processedImage = processedImage else { return }
+
+        let imageSaver = ImageSaver()
+
+        imageSaver.successHandler = {
+            print("Success!")
+        }
+
+        imageSaver.errorHandler = {
+            print("Oops: \($0.localizedDescription)")
+        }
+
+        imageSaver.writeToPhotoAlbum(image: processedImage)
+    }
+    
+    func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
+    func applyProcessing() {
+        let inputKeys = currentFilter.inputKeys
+
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(radiusIntensity * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+            processedImage = uiImage
+        }
+    }
+
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Shun Le Yi Mon on 24/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
